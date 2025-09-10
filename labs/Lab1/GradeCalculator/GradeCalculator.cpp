94040c4eeca62704992a39f0ab50c2bc490637e1
Cody Price <cody.price039@my.tccd.edu>
2025-09-10 14:05:12 -0500

    Setup Before 09/11/2025 Class Completed

--- labs/Lab1/GradeCalculator/GradeCalculator.cpp
+++ labs/Lab1/GradeCalculator/GradeCalculator.cpp
@@ -1,12 +1,53 @@
+/*
+Cody Price
+Lab 1
+COSC 1436 FALL 2025
+*/
+#include <iostream>
+#include <string>
 // GradeCalculator.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
-#include <string>
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+    std::cout << "Please enter your name:\n";
+    std::cout << "Please enter lab 1:\n";
+    std::cout << "Please enter lab 2:\n";
+    std::cout << "Please enter lab 3:\n";
+    std::cout << "Please enter lab 4:\n";
+    std::cout << "Please enter exam 1:\n";
+    std::cout << "Please enter exam 2:\n";
+    std::cout << "Please enter exam 3:\n";
+    std::cout << "Please enter participation:\n";
+    std::cout << "Please enter final exam:\n";
+    std::cout << "\n";
+    std::cout << " , your lab grades are:\n";
+        //Need to learn how to input the name prompted in "Enter your name"
+    std::cout << "Lab 1 = \n";
+    std::cout << "Lab 2 = \n";
+    std::cout << "Lab 3 = \n";
+    std::cout << "Lab 4 = \n";
+        //Need to learn how to input grades user entered in user input
+    std::cout << "\n";
+    std::cout << " , your exam grades are:\n";
+    std::cout << "Exam 1 = \n";
+    std::cout << "Exam 2 = \n";
+    std::cout << "Exam 3 = \n";
+    std::cout << "\n";
+    std::cout << " , your other grades are:\n";
+    std::cout << "Participation = \n";
+    std::cout << "Final Exam = \n";
+    std::cout << "\n";
+    std::cout << " , your class grade is:\n";
+    std::cout << "Lab Average (65%) = \n";
+    std::cout << "Exam Average (20%) = \n";
+    std::cout << "Participation (5%) = \n";
+    std::cout << "Final Exam (10%) = \n";
+    std::cout << "Class Average = \n";
+        //Need to learn how to input the math that will be run here
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
