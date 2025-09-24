e478736ac5ba01f408bb2a7f9b5f530d53cc580a
Cody Price <cody.price039@my.tccd.edu>
2025-09-23 21:23:45 -0500

    Logical operators

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -31,22 +31,45 @@ int main()
         std::cout << "Run length must be at least 0" << std::endl;
         runLength = 0;
     };
-
     std::cout << "Enter the release year (1900-2100): ";
     std::cin >> releaseYear;
     if (releaseYear < 1900)
-    {
+  /*  {
         std::cout << "Release year must be at least 1900" << std::endl;
         releaseYear = 1900;
-    };
+    } else if (releaseYear > 2100)
+
+    {
+        std::cout << "Release year must be between 1900 and 2100" << std::endl;
+        releaseYear = 1900;
+    }*/
+    if (releaseYear < 1900 || releaseYear > 2100)
+    {
+            std::cout << "Release year must be between 1900 and 2100" << std::endl;
+            releaseYear = 1900;
+    }
 
     std::cout << "Enter the optional description: ";
     std::getline(std::cin, description);
 
-    //TODO: Validate userRating
+    // Validate userRating
     std::cout << "Enter the optional user rating (1.0-10.0): ";
     std::cin >> userRating;
-
+    if (userRating < 1.0 || userRating > 10.0)
+    {
+        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
+        userRating < 1.0;
+    }
+   /* if (userRating < 1.0)
+    {
+        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
+        userRating <1.0;
+    } else if (userRating > 10.0)
+    {
+        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
+        userRating < 1.0;
+    }*/
+    
     std::cout << "Is this a classic (Y/N)? ";
     std::string input;
     std::cin >> input;
@@ -54,6 +77,8 @@ int main()
         isClassic = true;
     if (_strcmpi(input.c_str(), "N") == 0)
         isClassic = false;
+    else
+        std::cout << "You must enter either Y or N";
     //TODO: Handle other values
 }
 
