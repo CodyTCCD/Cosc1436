b9bc43a550d74a8fd62f7951b58ff72d4e640d9e
Cody Price <cody.price039@my.tccd.edu>
2025-09-25 21:14:27 -0500

    Switch discussion.

--- classwork/Chapter4/Chapter4/Chapter4.cpp
+++ classwork/Chapter4/Chapter4/Chapter4.cpp
@@ -4,9 +4,64 @@
  */
 #include <iostream>
 #include <string>
+#include <iomanip>
 
 int main()
 {
+    //Display Main Menu
+    std::cout << "Movie Library" << std::endl;
+    std::cout << "__________________" << std::endl;
+    std::cout << "A)dd Movie" << std::endl;
+    std::cout << "V)iew Movies" << std::endl;
+    std::cout << "E)dit Movie" << std::endl;
+    std::cout << "D)elete Movie" << std::endl;
+    std::cout << "Q)uit Movie" << std::endl;
+
+    char choice;
+    std::cin >> choice;
+
+    ////Validate input
+    //if (choice == "A" || choice == "a")
+    //{
+    //   //Movie add code here
+    //    std::cout << "Add not implemented" << std::endl;
+    //} else if (choice == "V" || choice == "V")
+    //{
+    //        //Movie view code here
+    //    std::cout << "Edit not implemented" << std::endl;
+    //} else if (choice == "E" || choice == "E")
+    //{
+    //    std::cout << "Delete not implemented" << std::endl;
+    //} else if (choice == "D" || choice == "D")
+    //{
+    //    std::cout << "Not implemented" << std::endl;
+    //} else if (choice == "Q" || choice == "Q")
+    //{
+    //     return 0;
+    //}else
+   /* {
+      std::cout << "Invalid Choice" << std::endl;
+    }*/
+    switch (choice)
+    {
+        case 'A': std::cout << "Add not implemented" << std::endl; break;
+        case 'a': std::cout << "Add not implemented" << std::endl; break;
+
+        case 'V': std::cout << "View not implemented" << std::endl; break;
+        case 'v': std::cout << "View not implemented" << std::endl; break;
+ 
+        case 'D': std::cout << "Delete not implemented" << std::endl; break;
+        case 'd': std::cout << "Delete not implemented" << std::endl; break;
+    
+        case 'E': std::cout << "Edit not implemented" << std::endl; break;
+        case 'e': std::cout << "Edit not implemented" << std::endl; break;
+    
+        case 'Q': return 0; break;
+        case 'q': return 0; break;
+
+        default: std::cout << "Invalid choice" << std::endl; break;
+    }
+
     //Movie details
     std::string title;          //Required
     std::string description;    //Optional
@@ -31,25 +86,30 @@ int main()
         std::cout << "Run length must be at least 0" << std::endl;
         runLength = 0;
     };
+
     std::cout << "Enter the release year (1900-2100): ";
     std::cin >> releaseYear;
-    if (releaseYear < 1900)
-  /*  {
+    /*if (releaseYear < 1900)
+    {
         std::cout << "Release year must be at least 1900" << std::endl;
         releaseYear = 1900;
     } else if (releaseYear > 2100)
-
     {
         std::cout << "Release year must be between 1900 and 2100" << std::endl;
         releaseYear = 1900;
     }*/
+
+    // Logical AND &&   Eb && Eb => bool
+    // Logical OR ||    Eb || Eb => bool
+    // Logical NOT !    !Eb => bool
     if (releaseYear < 1900 || releaseYear > 2100)
     {
-            std::cout << "Release year must be between 1900 and 2100" << std::endl;
-            releaseYear = 1900;
+        std::cout << "Release year must be between 1900 and 2100" << std::endl;
+        releaseYear = 1900;
     }
 
     std::cout << "Enter the optional description: ";
+    std::cin.ignore();
     std::getline(std::cin, description);
 
     // Validate userRating
@@ -57,29 +117,73 @@ int main()
     std::cin >> userRating;
     if (userRating < 1.0 || userRating > 10.0)
     {
-        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
-        userRating < 1.0;
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
     }
-   /* if (userRating < 1.0)
+    /*if (userRating < 1.0)
     {
-        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
-        userRating <1.0;
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
     } else if (userRating > 10.0)
     {
-        std::cout << "rating must be between 1.0 and 10.0" << std::endl;
-        userRating < 1.0;
+        std::cout << "Rating must be between 1.0 and 10.0" << std::endl;
+        userRating = 1.0;
     }*/
-    
+
     std::cout << "Is this a classic (Y/N)? ";
     std::string input;
     std::cin >> input;
-    if (_strcmpi(input.c_str(), "Y") == 0)
+
+    // if (Eb) S [ else S ] ;
+    // Else associates with nearest preceding if
+    // Nested if
+    //   if (E1) 
+    //     if (E2) 
+    //       if (E3)
+    //          S (E1, E2 and E3 must be true)
+    // if-else-if
+    //   if (E1)
+    //     S
+    //   else if (E2)
+    //     S  (E1 is false and E2 is true)
+    // 
+
+    /*if (_strcmpi(input.c_str(), "Y") == 0)
         isClassic = true;
     if (_strcmpi(input.c_str(), "N") == 0)
         isClassic = false;
     else
-        std::cout << "You must enter either Y or N";
+        std::cout << "You must enter either Y or N";*/
+    isClassic = false;
+    if (_strcmpi(input.c_str(), "Y") == 0)
+        isClassic = true;
+    else
+        if (_strcmpi(input.c_str(), "N") == 0)
+            isClassic = false;
+        else
+            std::cout << "You must enter either Y or N";
     //TODO: Handle other values
+
+    std::cout << std::fixed << std::setprecision(1) << std::endl; 
+    std::cout << title << " (" << releaseYear << ") " << std::endl;
+    std::cout << "Run Length " << runLength << " mins" << std::endl;
+    std::cout << "User Rating = " << userRating << std::endl;
+   /* if (isClassic)
+        std::cout << "Is Classic? Yes" << std::endl;
+    else
+        std::cout << "Is Classic? No" << std::endl;  */
+   /* std::string classicIndicator;
+    if (isClassic)
+        classicIndicator = "Yes";
+    else
+        classicIndicator = "No";*/
+//conditional_expression ::= Eb ? Et: Ef
+    /*std::string ClassicIndicator = IsClassic ? "Yes" : "No";*/
+    std::cout << "Is Classic? " << (isClassic ? "Yes" : "No") << std::endl;
+    if (description != "")
+        std::cout << description << std::endl;
+    std::cout << std::endl;
+
 }
 
 void relationalDemo()
