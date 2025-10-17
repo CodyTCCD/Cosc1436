15ec46162118f97f2a69c6e46dd758394fd186ac
Cody Price <cody.price039@my.tccd.edu>
2025-10-16 21:15:56 -0500

    Class 10/16/2025

--- classwork/Chapter6/Chapter6/Program.cpp
+++ classwork/Chapter6/Chapter6/Program.cpp
@@ -5,7 +5,6 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
-#include "Program.h"
 
 //Movie details
 struct Movie
@@ -18,55 +17,101 @@ struct Movie
     std::string genres;         //Optional (comma separated list of genres)
 };
 
+int g_thisIsAGlobalVariable = 100; //This is just an example. NEVER use global variables in ANY project
+
 /// <summary>Defines possible foreground colors.</summary>
 enum class ForegroundColor {
     Black = 30,
-    Red = 31, 
+    Red = 31,
     Green = 32,
     Yellow = 33,
-    Cyan = 36, 
+    Cyan = 36,
     BrightRed = 91,
     BrightGreen = 92,
     BrightYellow = 93,
     BrightCyan = 96
 };
 
+//Function prototypes
+//Forward declarations/referencing 
+void DisplayError(std::string);
+
 void ResetTextColor()
 {
     std::cout << "\033[0m";
 }
 
-void SetTextColor( ForegroundColor color)
+void SetTextColor(ForegroundColor color)
 {
     std::cout << "\033[" << (int)color << "m";
 }
 
-/// <summary> Displays an error message. <summary>
-/// <param name="message"> Message to display. <M/param>
-void DisplayError( std::string message )
+/// <summary>Display a confirmation message. </summary>
+/// <param name="message">Message to show.</param>
+/// <returns>Returns true or false depending on whther confirmed or not.</returns>
+bool Confirm(std::string message)
 {
-   // std::cout << "\033[91m" 
+    std::cout << message << " Y / N ) ";
+    std::string input;
+    std::cin >> input;
+
+    while (true)
+    {
+        if (_strcmpi(input.c_str(), "Y") == 0)
+            return true;
+        else if (_strcmpi(input.c_str(), "N") == 0)
+            return false;
+        else {
+            DisplayError("You must enter either Y or N");
+
+            std::cin >> input;
+        }
+    }
+}
+
+/// <summary>Displays an error message.</summary>
+/// <param name="message">Message to display.</param>
+void DisplayError(std::string message)
+{
+    //std::cout << "\033[91m" 
     SetTextColor(ForegroundColor::BrightRed);
     std::cout << "ERROR: " << message << std::endl;
     ResetTextColor();
 }
 
-/// <summary> Displays a warning message </summary>
-/// <param name="message"></param>
+/// <summary>Displays a warning message.</summary>
+/// <param name="message">Message to display.</param>
 void DisplayWarning(std::string message)
 {
-   // std::cout << "\033[93m"
     SetTextColor(ForegroundColor::BrightYellow);
     std::cout << message << std::endl;
     ResetTextColor();
 }
-/// <summary>View details of a movie.</summary>
-///<remarks>
-/// More details including paragraphs of data if you want.
-/// <remarks>
-void ViewMovie(Movie movie);
+
+std::string ReadString( std::string message, bool isRequired )
 {
-// View movie
+    std::cout << message;
+
+    std::string input;
+    std::getline(std::cin, input);
+
+    while (isRequired && input == "")
+    {
+        DisplayError("Value is required");
+
+        std::getline(std::cin, input);
+    }
+
+    return input; 
+}
+
+/// <summary>View details of a movie.</summary>
+/// <remarks>
+/// More details including paragraphs of data if you want.
+/// </remarks>
+void ViewMovie(Movie movie)
+{
+    // View movie
     //    Title (Year)
     //    Run Length # min
     //    User Rating = ##
@@ -80,24 +125,15 @@ void ViewMovie(Movie movie);
     if (movie.description != "")
         std::cout << movie.description << std::endl;
     std::cout << std::endl;
-};
+}
 
 /// <summary>Prompt user and add movie details.</summary>
-void AddMovie()
+Movie AddMovie ()
 {
-    Movie movie; // = {0};
+    Movie movie;// = {0};
 
     //Get movie details
-    std::cout << "Enter movie title: ";
-    std::cin.ignore();
-    std::getline(std::cin, movie.title);
-
-    //Title is required
-    while (movie.title == "")
-    {
-        DisplayError("Title is required");
-        std::getline(std::cin, movie.title);
-    }
+    movie.title = ReadString("Enter movie title: ", true);
 
     std::cout << "Enter the run length (in minutes): ";
     do
@@ -106,7 +142,8 @@ void AddMovie()
 
         //Error
         if (movie.runLength < 0)
-            DisplayError("Run length must be at least 0");        
+            DisplayError("Run length must be at least 0");
+
     } while (movie.runLength < 0);
 
     std::cout << "Enter the release year (1900-2100): ";
@@ -118,17 +155,13 @@ void AddMovie()
         std::cin >> movie.releaseYear;
     }
 
-    std::cout << "Enter the optional description: ";
-    std::cin.ignore();
-    std::getline(std::cin, movie.description);
+    movie.description = ReadString("Enter the optional description: ", false);
 
     // Genres, up to 5
     for (int index = 0; index < 5; ++index)
     {
-        std::string genre;
+        std::string genre = ReadString("Enter the genre (or blank to continue): ", false);
 
-        std::cout << "Enter the genre (or blank to continue): ";
-        std::getline(std::cin, genre);
         if (genre == "")
             break;
         else if (genre == " ")
@@ -137,26 +170,22 @@ void AddMovie()
         movie.genres = movie.genres + ", " + genre;
     }
 
-    std::cout << "Is this a classic (Y/N)? ";
-    std::string input;
-    std::cin >> input;
+    movie.isClassic = Confirm("Is this a classic movie?");
+    return movie; 
+}
 
-    while (true)
-    {
-        if (_strcmpi(input.c_str(), "Y") == 0)
-        {
-            movie.isClassic = true;
-            break;
-        } else if (_strcmpi(input.c_str(), "N") == 0)
-        {
-            movie.isClassic = false;
-            break;
-        } else {
-            DisplayError("You must enter either Y or N");
+void DeleteMovie(Movie movie)
+{
+    if (Confirm("Are you sure you want to delete " + movie.title + "?"))
+        return;
 
-            std::cin >> input;
-        }
-    }
+//TODO: Delete Movie
+    DisplayWarning("Not implemented yet");
+}
+
+void EditMovie(Movie movie)
+{
+    DisplayWarning("Not implemented yet");
 }
 
 int main()
@@ -181,16 +210,16 @@ int main()
         switch (choice)
         {
             case 'A':
-            case 'a': AddMovie(); break;
+            case 'a': movie = AddMovie(); break;
 
             case 'V':
-            case 'v': ViewMovie(); break;
+            case 'v': ViewMovie(movie); break;
 
             case 'D':
-            case 'd':DisplayWarning("Delete not implemented"); break;
+            case 'd': DeleteMovie(movie); break;
 
             case 'E':
-            case 'e':DisplayWarning("Edit not implemented"); break;
+            case 'e': EditMovie(movie); break;
 
             case 'Q':
             case 'q': done = true;
@@ -199,9 +228,7 @@ int main()
         };
     } while (!done);
 
-
-    // std::cin.ignore();
-
-    // Function call ::+ func ()
-    ViewMovie();
+    //std::cin.ignore();
+    // Function call ::= func () 
+    //ViewMovie();
 }
